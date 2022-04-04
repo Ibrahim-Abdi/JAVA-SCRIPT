65ca08707c8483b94d1e32da40275bae2ac284a7
Ibrahim Abdi <ibrahim10abdi@gmail.com>
2022-04-04 05:35:32 +0300

    on the sport

--- DO7/app.js
+++ DO7/app.js
@@ -1,42 +1,51 @@
-// ARRAYS
-// This a list of items
+//ARRAYS
+//This is a list of items;
 
-// declare an arrays
+//declare an array
 
 let users = ["Jane", "John", "Jack", "Jared", "Joy"];
 console.log(users);
 
-// access an item in the array -square bracket notation
-let sample = users[3];
+//accessing an item in the array - square bracket notation
+let sampleUser = users[1];
+console.log(sampleUser);
 
-// get the length
+//get the length(count the number of items in the array)
 let numOfUsers = users.length;
+console.log(numOfUsers);
 
-// add a new item to the array
-// at the end --push
+//get an element from the end of the array
+let lastItem = users[users.length - 1];
+console.log(lastItem);
+
+//add an item to the array
+// - at the end
 users.push("Jayden");
 console.log(users);
 
-// at the beginning
+// - at the begining
 users.unshift("JJ");
 console.log(users);
 
 let myForm = document.getElementById("userForm");
 
-// events
+users = [];
 myForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    let newUser = document.getElementById("userInput").value;
-   users.push(newUser);
-   addUser(users);
+  e.preventDefault();
+  let userInput = document.getElementById("userInput").value;
+  users.unshift(userInput);
+  addNewUser(users);
+  document.getElementById("userInput").value = "  ";
 });
 
-function addUser(users) {
-    let markup = [];
-    for (let i = 0; i < users.length; i++) {
-        markup.push('<li>${users[i]}</li>');
-    }
-    let list = document.getElementById("userList");
-    list.innerHTML = markup.join(" ");
+function addNewUser(users) {
+  let markup = [];
+  for (let i = 0; i < users.length; i++) {
+    let listItem = `<li class="border-solid border-b-2 p-3 text-md">${users[i]}</li>`;
+    markup.push(listItem);
+  }
+  let userList = document.getElementById("userList");
+
+  userList.innerHTML = markup.join(" ");
 }
-addUser(users);
+addNewUser(users);
