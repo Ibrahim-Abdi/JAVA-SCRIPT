6e21a4bab20569accf34a1e5e0b795c609463186
Ibrahim Abdi <ibrahim10abdi@gmail.com>
2022-04-07 08:37:04 +0300

    Into-of-Java-Scripts

---
+++ DO 1/app.js
@@ -0,0 +1,85 @@
+console.log("Hello there");
+
+/**
+ * Variables
+ * a container of data (identifier)
+ */
+
+//declare a variable
+var username = "Ibrahim";
+// let lastName = "Abdi";
+
+//assigning values to a variable
+// username = "ibrahim";
+// lastName = "abdi";
+
+//use variable
+console.log(username);
+console.log(lastName);
+
+//re-asigning a variable
+username = "jack";
+console.log(username);
+
+/**
+ * RULES OF NAMING VARIABLES
+ * 1. Variables can only contain letters, underscores or dollar signs
+ * 2. Even though the variables can contain numbers they cannot start with a number
+ * 3. you cannot re declare variables
+ * 4. variables should not have spaces
+ * 5. variables should not be js keywords
+ * 6. variables are case sensitive
+ */
+
+//kebab case
+let current_user_info;
+
+//camel case
+let currentUserInfo;
+
+// let age = 67;
+// let Age = 56;
+
+/**
+ * DATA TYPES
+ */
+
+/**
+ * String - textual content
+ *
+ */
+let message = "Hello how are you";
+
+/**
+ * number
+ */
+let Age = 56;
+let height = 8.9;
+
+/**
+ * Boolean  - true or false
+ *
+ */
+let isRaining = true;
+let isAuth = false;
+
+/**
+ * Declare relevant variables according to the following scenario
+ * John Juma of age 45, is a new student doing software development course. He has a high score of 89.87 and his nickname is johny
+ */
+
+ HEAD
+var f_Name = "John";
+var l_name = "Juma";
+var number = 45;
+var course = "software development";
+var exam_score = 89.87;
+var nickname = "johnny";
+// =======
+let firstName = "John";
+let lastName = "Juma";
+let course = "software development";
+let age = 45;
+let highScore = 89.87;
+let nickname = "johnny";
+let isNewStudent = true;
